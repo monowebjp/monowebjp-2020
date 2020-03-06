d143555d08b2b4cdec0aea9a5ea87a70cb679072
Nakamura Masumi <capybara@monoweb.jp>
2020-03-06 10:42:00 +0900

    styleguideの記述調整中

--- fractal.js
+++ fractal.js
@@ -1,16 +1,21 @@
 'use strict';
 
+const consolidate = require('@frctl/consolidate');
+const path = require('path');
+
 /* Create a new Fractal instance and export it for use elsewhere if required */
 const fractal = module.exports = require('@frctl/fractal').create();
+const ejsAdapter = consolidate('ejs');
 
 /* Set the title of the project */
 fractal.set('project.title', 'monoweb.jp');
 
 /* File extentions */
-fractal.components.set('ext', '.html');
+fractal.components.engine(ejsAdapter);
+fractal.components.set('ext', '.ejs');
 
 /* Tell Fractal where the components will live */
-fractal.components.set('path', __dirname + '/styleguide');
+fractal.components.set('path', __dirname + '/src/objects');
 // fractal.web.set('builder.dest', __dirname + '/build');
 /* Tell Fractal where the documentation pages will live */
 fractal.docs.set('path', __dirname + '/src/docs');
