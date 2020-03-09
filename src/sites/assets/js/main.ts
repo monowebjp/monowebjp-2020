1d6c96ac55497f2936aff07c17857f0a16ba3750
Nakamura Masumi <capybara@monoweb.jp>
2020-03-09 15:59:52 +0900

    script記述開始

---
+++ src/sites/assets/js/main.ts
@@ -0,0 +1,6 @@
+import {shareFacebook, shareHatena, shareLine, shareTwitter} from './share';
+
+(window as any).shareFacebook = shareFacebook;
+(window as any).shareTwitter = shareTwitter;
+(window as any).shareLine = shareLine;
+(window as any).shareHatena = shareHatena;
---
+++ src/sites/assets/js/share.ts
@@ -0,0 +1,15 @@
+export const shareFacebook = () => {
+    alert('facebook');
+};
+
+export const shareTwitter = () => {
+    alert('twitter');
+};
+
+export const shareLine = () => {
+    alert('LINE');
+};
+
+export const shareHatena = () => {
+    alert('Hatena');
+};
