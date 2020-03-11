13c25ab655e2e6b6cd3461ab6413fb2bfcc5a754
Nakamura Masumi <capybara@monoweb.jp>
2020-03-11 10:29:03 +0900

    #2 シェアボタン実装 facebookのみステージングで確認が必要

--- src/sites/assets/js/share.ts
+++ src/sites/assets/js/share.ts
@@ -1,34 +1,34 @@
-const url = location.href;
+const url = encodeURIComponent(location.href);
+const pageTitle = encodeURIComponent(document.title);
+
+const shareWindow = (shareUrl: string) => {
+    window.open(
+        shareUrl,
+        '',
+        'width=580,height=400,menubar=no,toolbar=no,scrollbars=yes'
+    );
+}
 
 export const shareFacebook = (): boolean => {
     const shareUrl = `//www.facebook.com/share.php?u=${url}`;
-    console.log(shareUrl);
-    // window.open(
-    //     shareUrl,
-    //     '',
-    //     'width=580,height=400,menubar=no,toolbar=no,scrollbars=yes'
-    // );
+    shareWindow(shareUrl);
     return true;
 };
 
 export const shareTwitter = (): boolean => {
-    const shareUrl = `//twitter.com/share?text=${encodeURIComponent(
-        'aaa'
-    )}&url=${url}`;
-    console.log(shareUrl);
+    const shareUrl = `//twitter.com/share?text=${pageTitle}&url=${url}`;
+    shareWindow(shareUrl);
     return true;
 };
 
 export const shareLine = (): boolean => {
-    const shareUrl = `//line.me/R/msg/text/?${encodeURIComponent(
-        'aaa'
-    )} ${encodeURIComponent(url)}`;
-    console.log(shareUrl);
+    const shareUrl = `//line.me/R/msg/text/?${pageTitle} ${url}`;
+    shareWindow(shareUrl);
     return true;
 };
 
 export const shareHatena = (): boolean => {
-    const shareUrl = `//b.hatena.ne.jp/entry/panel/?url=${url}&title=${'pagetitle'}`;
-    console.log(shareUrl);
+    const shareUrl = `//b.hatena.ne.jp/entry/panel/?url=${url}&title=${pageTitle}`;
+    shareWindow(shareUrl);
     return true;
 };
